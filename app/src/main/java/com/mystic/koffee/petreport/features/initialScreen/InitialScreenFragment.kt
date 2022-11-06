f02980c8a7a285f094f80a9e886b32b1fe60e112
jaumpeliconjera <joaocarlos@jeraapps.com>
2022-11-06 18:25:27 -0400

    [UPDATE] Create add report dialog

--- app/src/main/java/com/mystic/koffee/petreport/features/initialScreen/InitialScreenFragment.kt
+++ app/src/main/java/com/mystic/koffee/petreport/features/initialScreen/InitialScreenFragment.kt
@@ -1,18 +1,45 @@
 package com.mystic.koffee.petreport.features.initialScreen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.mystic.koffee.petreport.R
+import com.mystic.koffee.petreport.databinding.FragmentInitialScreenBinding
+import com.mystic.koffee.petreport.support.ui.AddReportDialog
 
-class InitialScreenFragment : Fragment() {
+class InitialScreenFragment : Fragment(R.layout.fragment_initial_screen) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_initial_screen, container, false)
+
+    /**
+    Fragment life cycle
+     **/
+    private var _binding: FragmentInitialScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentInitialScreenBinding.bind(view)
+        setup()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setup() {
+        setupAddFloatActionButton()
+    }
+
+    private fun setupAddFloatActionButton() {
+        binding.addReportFloatingActionButton.setOnClickListener {
+            showAddReportDialog()
+        }
+    }
+
+    private fun showAddReportDialog() {
+        childFragmentManager.let {
+            AddReportDialog(addReportCallback = { print("teste") }).show(it, null)
+        }
     }
 }
---
+++ app/src/main/java/com/mystic/koffee/petreport/support/ui/AddReportDialog.kt
@@ -0,0 +1,37 @@
+package com.mystic.koffee.petreport.support.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mystic.koffee.petreport.databinding.AddReportDialogBinding
+
+class AddReportDialog (
+    private val addReportCallback: () ->  Unit,
+) : DialogFragment() {
+    private lateinit var binding: AddReportDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = AddReportDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    private fun setup() {
+        with(binding) {
+            addReportButton.setOnClickListener {
+                addReportCallback()
+                dismiss()
+            }
+        }
+    }
+}
